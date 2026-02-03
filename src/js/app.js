92fdb9655cb6a12d4007fdb6f4a94b7b3c0b0cc0
German Velasco <gvelascopedreros@gmail.com>
2026-02-03 10:23:10 -0500

    Estructura HTML,CSS,JS

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+const button = document.getElementById("cta-btn");
+const form = document.getElementById("contact-form");
+
+button.addEventListener("click", () => {
+    alert("¡Gracias por tu interés!");
+});
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("Formulario enviado correctamente");
+    form.reset();
+});
